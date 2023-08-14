a624d93fc17a3dc512b442b6888787905b618d86
Fewatts <feralveswatts@gmail.com>
2023-08-14 16:54:36 -0300

    Calculation orders OK

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
+/**
+ * Get the display element.
+ * @type {HTMLElement}
+ */
 const display = document.getElementById("display");
+
+/**
+ * Get all buttons with the "btn" class.
+ * @type {NodeList}
+ */
 const buttons = document.querySelectorAll(".btn");
 
 /**
- * Performs arithmetic calculations based on given numbers and an operator.
- *
+ * Perform arithmetic calculations based on given numbers and an operator.
  * @param {number} num1 - The first number.
- * @param {string} operator - The arithmetic operator (+, -, *, /).
+ * @param {string} operator - The arithmetic operator (+, -, *, /, %).
  * @param {number} num2 - The second number.
  * @returns {string} The formatted result of the arithmetic operation with up to 2 decimal places if necessary,
  *          or an error message if invalid.
  */
-const calculate = (num1, operator, num2) => {
-   const result = performCalculation(num1, operator, num2);
-   return formatResult(result);
-};
+const calculate = (num1, operator, num2) =>
+   formatResult(performCalculation(num1, operator, num2));
 
 /**
- * Performs the actual arithmetic calculation based on given numbers and an operator.
- *
+ * Perform the actual arithmetic calculation based on given numbers and an operator.
  * @param {number} num1 - The first number.
  * @param {string} operator - The arithmetic operator (+, -, *, /).
  * @param {number} num2 - The second number.
@@ -30,10 +35,7 @@ const performCalculation = (num1, operator, num2) => {
       case "-":
          return num1 - num2;
       case "/":
-         if (num2 === 0) {
-            return "Error.";
-         }
-         return num1 / num2;
+         return num2 === 0 ? "Error." : num1 / num2;
       case "*":
          return num1 * num2;
       case "%":
@@ -44,8 +46,7 @@ const performCalculation = (num1, operator, num2) => {
 };
 
 /**
- * Formats the calculation result, removing decimal places if they're zero.
- *
+ * Format the calculation result, removing decimal places if they're zero.
  * @param {number} result - The unformatted result of the arithmetic operation.
  * @returns {string} The formatted result with up to 2 decimal places if necessary.
  */
@@ -55,51 +56,42 @@ const formatResult = (result) => {
    }
    const formattedResult = parseFloat(result).toFixed(2);
    const [integerPart, decimalPart] = formattedResult.split(".");
-   if (parseInt(decimalPart) === 0) {
-      return integerPart;
-   } else {
-      return formattedResult;
-   }
+   return parseInt(decimalPart) === 0 ? integerPart : formattedResult;
 };
 
 /**
- * Handles button click events and updates the calculator display.
- *
+ * Handle button click events and update the calculator display.
  * @param {string} input - The input received from the clicked button.
  */
 const handleButtonClick = (input) => {
    if (input === "=") {
-      // Perform the calculation using the current input
-      const result = calculateFromString(display.textContent);
-      display.textContent = result;
+      display.textContent = calculateFromString(display.textContent);
    } else if (input === "C") {
-      // Handle clear
       display.textContent = "0";
-   } else if (display.textContent === "0") {
-      // Start a new input
-      display.textContent = input;
-   } else if (display.textContent === "Error.") {
-      // Handle input after an error
+   } else if (display.textContent === "0" || display.textContent === "Error.") {
       display.textContent = input;
    } else if (["+", "-", "*", "/", "%"].includes(input)) {
-      // Store the operator for the next calculation
-      display.textContent += ` ${input} `;
-   } else if (input === "←") {
-      if (display.textContent === "0") {
-         return;
+      const lastChar = display.textContent.slice(-1);
+      const inputWithoutOperator = display.textContent;
+      if (!isNaN(lastChar)) {
+         display.textContent =
+            calculateFromString(inputWithoutOperator) + ` ${input} `;
+      } else {
+         display.textContent = inputWithoutOperator.slice(0, -2) + ` ${input} `;
       }
-      handleBackspace();
+   } else if (input === "←") {
+      if (display.textContent !== "0") {
+         handleBackspace();
+      }
    } else if (input === ".") {
       handleDecimalInput();
    } else {
-      // Continue the current input
       display.textContent += input;
    }
 };
 
 /**
- * Calculates a result based on a string input in the valid format.
- *
+ * Calculate a result based on a string input in the valid format.
  * @param {string} input - The input string in the valid format: "num1 operator num2".
  * @returns {string} The formatted result of the calculation, showing two decimal places if needed,
  *          or an error message if the input is invalid.
@@ -107,10 +99,8 @@ const handleButtonClick = (input) => {
 const calculateFromString = (input) => {
    const expressions = input.split(" ");
    let result = parseFloat(expressions[0]);
-   let operator = null;
-
    for (let i = 1; i < expressions.length; i += 2) {
-      operator = expressions[i];
+      const operator = expressions[i];
       const num2 = parseFloat(expressions[i + 1]);
       result = calculate(result, operator, num2);
    }
@@ -118,21 +108,22 @@ const calculateFromString = (input) => {
 };
 
 /**
- * Function to handle decimal point
+ * Handle decimal point input.
  */
 const handleDecimalInput = () => {
    const currentContent = display.textContent;
    const lastChar = currentContent.slice(-1);
-   if (!currentContent.includes(".")) {
-      // Only allow adding decimal point if the last character is a number
-      if (!isNaN(lastChar) || lastChar === ".") {
-         display.textContent += ".";
-      }
+   if (
+      !currentContent.includes(".") &&
+      (!isNaN(lastChar) || lastChar === ".")
+   ) {
+      display.textContent += ".";
    }
 };
 
 /**
- * Function to handle keyboard input
+ * Handle keyboard input events.
+ * @param {KeyboardEvent} event - The keyboard event.
  */
 const handleKeyboardInput = (event) => {
    const key = event.key;
@@ -140,17 +131,12 @@ const handleKeyboardInput = (event) => {
       handleButtonClick("=");
    } else if (key === "Escape") {
       handleButtonClick("C");
-   } else if (/[0-9]/.test(key)) {
+   } else if (/[0-9]/.test(key) || /[\+\-\*/%]/.test(key) || key === ".") {
       handleButtonClick(key);
-   } else if (/[\+\-\*/%]/.test(key)) {
-      handleButtonClick(key);
-   } else if (key === ".") {
-      handleButtonClick(".");
    } else if (key === "Backspace") {
-      if (display.textContent === "0") {
-         return;
+      if (display.textContent !== "0") {
+         handleButtonClick("←");
       }
-      handleButtonClick("←");
    }
 };
 
@@ -158,16 +144,12 @@ const handleKeyboardInput = (event) => {
 document.addEventListener("keydown", handleKeyboardInput);
 
 /**
- * Function to handle backspace
+ * Handle backspace input.
  */
 const handleBackspace = () => {
    const currentContent = display.textContent;
-   const newContent = currentContent.slice(0, -1);
-   if (newContent === "") {
-      display.textContent = "0";
-   } else {
-      display.textContent = newContent;
-   }
+   const newContent = currentContent.slice(0, -1) || "0";
+   display.textContent = newContent;
 };
 
 // Add event listener for buttons
